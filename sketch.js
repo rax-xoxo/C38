2213395d829d71c06ad1d8bb1b8a814cd1180d62
Rashi Sharma <69389235+Rashi260@users.noreply.github.com>
2020-12-02 16:48:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,12 +13,12 @@ var cars, car1, car2, car3, car4;
 var car1img, car2img, car3img, car4img, trackimg, groundimg;
 
 function preload(){
-  car1img= loadImage("images/car1.png");
-  car2img= loadImage("images/car2.png");
-  car3img= loadImage("images/car3.png");
-  car4img= loadImage("images/car4.png");
-  trackimg = loadImage("images/track.jpg");
-  groundimg= loadImage("images/ground.png");
+  car1img= loadImage("car1.png");
+  car2img= loadImage("car2.png");
+  car3img= loadImage("car3.png");
+  car4img= loadImage("car4.png");
+  trackimg = loadImage("track.jpg");
+  groundimg= loadImage("ground.png");
 }
 
 function setup(){
